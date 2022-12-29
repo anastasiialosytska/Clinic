a37bd884832012bd1ae2b5b82e55b5372a7c78d0
Anastasiia Losytska <alosytska@gmail.com>
2022-12-29 17:17:03 +0200

    Add localization

--- app/models/doctor_visit.rb
+++ app/models/doctor_visit.rb
@@ -2,6 +2,8 @@ class DoctorVisit < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
 
+  validates :appointment, length: { minimum: 20 }, on: :update
+
   enum status: {
     open: 0,
     closed: 1
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Clinic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    I18n.config.available_locales = [:en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # Configuration for the application, engines, and railties goes here.
     #
