78c0ef4a69294682d02f340ba23983bbc0967056
anastasiialosytska <anastasiia.losytska@rubygarage.org>
2024-10-03 17:04:43 +0200

    Change rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,10 +26,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
-  Capybara.default_driver = :selenium_chrome
-  Capybara.javascript_driver = :selenium_chrome
-  Capybara.automatic_label_click = true
-
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
